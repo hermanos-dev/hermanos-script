bed8b24abd06fb304493883bf10b734abeef52b7
Hermanos <89389178+hermanos-dev@users.noreply.github.com>
2024-03-11 01:01:33 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local main = function()
     local t = 0
-    local u = 'https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/test.lua'
+    local u = 'https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/main.lua'
 
     while true do task.wait()
         if t >= 10 or getgenv().script_h then return end
